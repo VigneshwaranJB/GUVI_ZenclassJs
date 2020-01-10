6f7548a24120316e8b00bff317f9913def990a2e
VIgneshwaran Balamurugan <58186438+VigneshwaranJB@users.noreply.github.com>
2020-01-10 06:36:59 +0530

    Add files via upload

---
+++ Cricket/stat.ts
@@ -0,0 +1,52 @@
+let tem1PlyRuns = localStorage.getItem("team1Runs").split(',');
+let tem2PlyRuns = localStorage.getItem("team2Runs").split(',');
+let tem1Total = localStorage.getItem("team1Total");
+let tem2Total = localStorage.getItem("team2Total");
+let tem1PlyObj = JSON.parse(localStorage.getItem("playerObj"));
+let tem2PlyObj = JSON.parse(localStorage.getItem("player2Obj"));
+let tem1HighRun =0;
+let tem2HighRun =0;
+
+function matchResult(){
+    tem1HighRun = parseInt(tem1PlyRuns[0]);
+    for(i=0; i<tem1PlyRuns.length - 1; i++){
+        if( tem1HighRun < parseInt(tem1PlyRuns[i+1]) ){
+            tem1HighRun = parseInt(tem1PlyRuns[i+1]);
+        }
+    }
+    //alert(tem1HighRun);
+
+    tem2HighRun = parseInt(tem2PlyRuns[0]);
+    for(i=0; i<tem2PlyRuns.length - 1; i++){
+        if(tem2HighRun < parseInt(tem2PlyRuns[i+1]) ){
+            tem2HighRun = parseInt(tem2PlyRuns[i+1]);
+        }
+    }
+    //alert(tem2HighRun);
+    
+    for(i=0; i<10; i++){
+        let rid = "r"+i;
+        let bid ="b"+i;
+        document.getElementById(rid).innerHTML = ((tem1PlyObj[i].total == 0) ? "0" : tem1PlyObj[i].total);
+        document.getElementById(bid).innerHTML = ((tem1PlyObj[i].balls == 6 && tem1PlyObj[i].total == 0) ? "Yet to bat" : (6 - tem1PlyObj[i].balls));
+    }
+    document.getElementById("t1").innerHTML = localStorage.getItem("team1Total");
+    
+    //document.getElementById("matchSummary").innerHTML += "<span>Highest run scorer: "+tem1HighRun+"</span><br><br><br>";  
+    
+    for(i=0; i<10; i++){
+        let rid = "rr"+i;
+        let bid ="bb"+i;
+        document.getElementById(rid).innerHTML = ((tem2PlyObj[i].total == 0) ? "0" : tem2PlyObj[i].total);
+        document.getElementById(bid).innerHTML = ((tem2PlyObj[i].balls == 6 && tem2PlyObj[i].total == 0) ? "Yet to bat" : (6 - tem2PlyObj[i].balls));
+    }
+    document.getElementById("t2").innerHTML = localStorage.getItem("team2Total");
+    document.getElementById("result").innerHTML = "<h2>"+localStorage.getItem("winner")+"</h2>";
+    
+    
+    //document.getElementById("matchSummary").innerHTML += "<span>Highest run scorer: "+tem2HighRun+"</span><br>";
+
+    let mofMatch = (tem1HighRun > tem2HighRun) ? tem1HighRun : tem2HighRun;
+    document.getElementById("result").innerHTML += "<h2>Man of the match: "+mofMatch+"</h2>";
+
+}
