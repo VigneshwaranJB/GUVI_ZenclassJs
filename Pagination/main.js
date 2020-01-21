1a9e00e47d28f9f8d1b28bde46e422182e6f216b
VIgneshwaran Balamurugan <58186438+VigneshwaranJB@users.noreply.github.com>
2020-01-21 10:50:16 +0530

    Add files via upload

--- Pagination/main.js
+++ Pagination/main.js
@@ -1,4 +1,4 @@
-const userList=[
+const list_items = [
     {
       "id": "1",
       "name": "Lura Senger",
@@ -501,51 +501,59 @@ const userList=[
     }
   ];
 
-const userElements=document.getElementById('list');
-const paginationElements=document.getElementById('pagenation');
+const list_element = document.getElementById('list');
+const pagination_element = document.getElementById('pagination');
 
-let currentPage=1;
-let rows=5;
+let current_page = 1;
+let rows = 5;
 
-function displayList(items,wrapper,rowsPerPage,page){
-    wrapper.innerHTML="";
-    page--;
+function DisplayList (items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = "";
+	page--;
 
-    let start=rowsPerPage*page;
-    let end=start+rowsPerPage;
-    let paginatedItems=items.slice(start,end);
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+	let paginatedItems = items.slice(start, end);
 
+	for (let i = 0; i < paginatedItems.length; i++) {
+		let item = paginatedItems[i];
 
-    for(let i=0;i<paginatedItems.length;i++){
-        let item=paginatedItems[i];
-
-        let userElements=document.createElement('div');
-        userElements.classList.add('item');
-        userElements.innerText=item.id;
-
-        wrapper.appendChild(userElements);
-    }
+		let item_element = document.createElement('div');
+		item_element.classList.add('item');
+		item_element.innerText = item.name;
+		
+		wrapper.appendChild(item_element);
+	}
 }
 
-function SetupPagination(items,wrapper,rowsPerPage){
-    wrapper.innerHTML="";
+function SetupPagination (items, wrapper, rows_per_page) {
+	wrapper.innerHTML = "";
 
-    let pageCount=Math.ceil(items.length/rowsPerPage);
-    for(let i=1;i<pageCount+1;i++){
-        let btn = PaginationButton(i);
-        wrapper.appendChild(btn);
-    }
+	let page_count = Math.ceil(items.length / rows_per_page);
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, items);
+		wrapper.appendChild(btn);
+	}
 }
 
-function PaginationButton(page){
-    let button=document.createElement('button');
-    button.innerText=page;
+function PaginationButton (page, items) {
+	let button = document.createElement('button');
+	button.innerText = page;
 
-    if(currentPage==page) button.classList.add('active');
-    
-    return button;
+	if (current_page == page) button.classList.add('active');
+
+	button.addEventListener('click', function () {
+		current_page = page;
+		DisplayList(items, list_element, rows, current_page);
+
+		let current_btn = document.querySelector('.pagenumbers button.active');
+		current_btn.classList.remove('active');
+
+		button.classList.add('active');
+	});
+
+	return button;
 }
 
-
-displayList(userList,userElements,rows,currentPage);
-SetupPagination(userList,paginationElements,rows);
+DisplayList(list_items, list_element, rows, current_page);
+SetupPagination(list_items, pagination_element, rows);
